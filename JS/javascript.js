970d19af8b45c49faa83fbdfaec0f1e76a2d938c
tomas <tomyec98@gmail.com>
2023-02-28 21:53:26 -0300

    ultimo intento

---
+++ JS/javascript.js
@@ -0,0 +1,23 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    const nav = document.querySelector("nav");
+    const abrir = document.querySelector("#abrir")
+    const cerrar = document.querySelector("#cerrar");
+
+    abrir.addEventListener("click", () => {
+        nav.classList.add("visible");
+    });
+
+    cerrar.addEventListener("click", () => {
+        nav.classList.remove("visible");
+    });
+    
+});
+
+
+
+document.addEventListener("click", (event) => {
+
+});
+
+
